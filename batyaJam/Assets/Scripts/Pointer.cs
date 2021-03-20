90fff27d0712a31a707308a4bf3da8d47a56bfa9
vvr <vover@mail.ru>
2021-03-20 11:30:38 +0300

    made it urp

--- batyaJam/Assets/Scripts/Pointer.cs
+++ batyaJam/Assets/Scripts/Pointer.cs
@@ -8,22 +8,34 @@ public class Pointer : MonoBehaviour
     [SerializeField] private Transform pointerTransform;
     private Camera mainCamera;
     [SerializeField] LayerMask groundLayer;
+    private bool click;
 
     void Awake()
     {
         mainCamera = Camera.main;
     }
 
-
-    private void OnMouseDown()
+    private void Update()
     {
-        
-        RaycastHit rayHit;
-        if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, groundLayer))
+        if (Input.GetMouseButtonDown(0))
         {
-            pointerTransform.position = rayHit.point;
-            
+            RaycastHit rayHit;
+            if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, groundLayer))
+            {
+                Debug.Log("hit");
+                pointerTransform.position = rayHit.point;
+            }
         }
     }
+    //private void OnMouseDown()
+    //{
+
+    //    RaycastHit rayHit;
+    //    if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, groundLayer))
+    //    {
+    //        pointerTransform.position = rayHit.point;
+
+    //    }
+    //}
 
 }
